151f29ab5f9a6f73b8b0420c652d4b2eb246e067
Samy <samyking.sb@gmail.com>
2021-06-07 01:15:40 +0200

    File System and Command Line Args (Notes App)
    chapter 4, lesson 4

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,54 @@
 const chalk = require ('chalk')
+const { argv, demandOption } = require('yargs')
+const yargs = require ('yargs')
 const notes = require ('./notes')
 
+// Create add command
+yargs.command({
+    command: "add",
+    description: "Add a new note",
+    builder:{
+        title:{
+            describe: "Note title",
+            demandOption:true,
+            type:"string"
+        },
+        body:{
+            describe: "Note Body",
+            demandOption:true,
+            type:"string"
+        }
+    },
+    handler: function(argv){
+        console.log("Title: " + argv.title+"\n Body: "+argv.body)
+    }
+})
 
-console.log(notes())
-console.log(chalk.red.bold.inverse('Error 404!'))
+// Create remove command
+yargs.command({
+    command: "remove",
+    description: "Remove a note",
+    handler: function(){
+        console.log("Removeing a note...")
+    }
+})
+
+// Create list command
+yargs.command({
+    command: "list",
+    description: "list the note",
+    handler: function(){
+        console.log("Listing a note...")
+    }
+})
+
+// Create read command
+yargs.command({
+    command: "read",
+    description: "Read a note",
+    handler: function(){
+        console.log("Reading a note...")
+    }
+})
+
+yargs.parse()
