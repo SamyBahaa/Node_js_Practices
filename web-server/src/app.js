60101e25466c3e8f676db0c04c9c92a12d9785b1
Samy <samyking,sb@gmail.com>
2021-07-30 21:23:40 +0200

    Dynamic Pages with Templating
    ch: 7 , l: 6 , m:10:07

---
+++ web-server/src/app.js
@@ -0,0 +1,22 @@
+const path = require("path")
+const express = require("express")
+ 
+const app = express()
+const publicDirectoryPath = path.join(__dirname,'../public')
+
+app.set('view engine','hbs')
+app.use(express.static(publicDirectoryPath))
+
+
+app.get('', (req, res) => {
+    res.render('index')
+})
+
+// app.get('/weather', (req, res) => {
+//     res.send("Weather page!")
+// })
+
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
